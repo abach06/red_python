a2f3104ac670a90392ddb8073918ed0aaddeca0b
abach06 <abach06@gmail.com>
2023-02-09 05:17:28 +0000

    Updated Bio.py, Hello World.py

--- Bio.py
+++ Bio.py
@@ -1,3 +1,7 @@
 name = input("What is your name? ")
 color = input("What is your favorite color? ")
 age = int(input("How old are you today? "))
+
+print(name, end=" ")
+print("is " + str(age) + " years old", end=" ")
+print("and loves the color " + color + ".", end=" ")
