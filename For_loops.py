4c5eef6439253d37c852e3f7808cf9afa2e48470
abach06 <abach06@gmail.com>
2023-02-11 07:16:55 +0000

    loops

---
+++ For_loops.py
@@ -0,0 +1,20 @@
+# For loops
+
+# Most common type of loop
+
+#colors = ['blue', 'green', 'red', 'purple']
+#for color in colors:
+ #   print(color)
+
+point = (1, 2, 3)
+for value in point:
+    print(value)
+
+ages = {'kevin': 59, 'bob': 61, 'alesha': 27}
+for key in ages:
+    print(key)
+    
+for key, value in ages.items():
+    print(key, value)
+    
+    
