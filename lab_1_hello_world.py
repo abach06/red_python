31419e20bc8a8fa5e92561482dfcbe1fda1bc1dd
abach06 <abach06@gmail.com>
2023-02-22 10:44:35 +0000

    lab_1_hello_world

---
+++ lab_1_hello_world.py
@@ -0,0 +1,27 @@
+#Functions
+
+#declare a function by using syntax "def function_name():"
+
+# A function that prints hello world
+
+#def hello_world():
+   # print('hello world')
+    
+    
+## This line calls (runs) the function
+#hello_world()
+
+
+#Modify lab_1_hello_world.py as follows:
+
+# A function that returns hello world
+def hello_world():
+    return 'hello world'
+    
+## Assign the hello_world() function to a variable.
+greeting = hello_world()
+print(greeting)
+
+    
+    
+    
