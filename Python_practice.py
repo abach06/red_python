751486d33bb701faa2c8c26c8a3dfdb9b3f81a4c
abach06 <abach06@gmail.com>
2023-02-22 10:02:28 +0000

    lists

--- Python_practice.py
+++ Python_practice.py
@@ -7,5 +7,26 @@ fruit = ["oranges", "apples", "bananas"]
 #print(fruit[1])
 #print(fruit[2])
 
-print(fruit[-1])
+#print(fruit[-1])
+
+fruit.append("kiwi")
+#print(fruit)
+
+fruit.insert(2, "passion fruit")
+#print(fruit)
+
+print(sorted(fruit))
+print(fruit)
+
+#fruit.reverse()
+#print(fruit)
+
+del(fruit[1])
+print(fruit)
+
+favorite_fruit = fruit.pop()
+print(favorite_fruit)
+
+
+
 
