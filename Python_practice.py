25dd92614c80575b1ff6dafc76b285845a6080c0
abach06 <abach06@gmail.com>
2023-02-22 07:34:27 +0000

    lists

--- Python_practice.py
+++ Python_practice.py
@@ -1,10 +1,11 @@
 #lists
 
-#fruit = ["oranges", "apples", "bananas"]
+fruit = ["oranges", "apples", "bananas"]
 
 #print(fruit)
 #print(fruit[0])
 #print(fruit[1])
 #print(fruit[2])
 
+print(fruit[-1])
 
